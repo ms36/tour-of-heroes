dce7f4474d93770e633fc65c8f3cf981f47d5087
ms36 <mstevenson36@hotmail.com>
2019-12-04 23:48:38 -0500

    finished

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Cyclops' },
+  { id: 13, name: 'Beast' },
+  { id: 14, name: 'Nightcrawler' },
+  { id: 15, name: 'Ice Man' },
+  { id: 16, name: 'Rogue' },
+  { id: 17, name: 'Storm' },
+  { id: 18, name: 'Professor X' },
+  { id: 19, name: 'Jean Grey' },
+  { id: 20, name: 'Collossus' }
+];
